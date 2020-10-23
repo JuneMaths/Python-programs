481988da42a758fb21149c162e86427d68148da8
JuneMaths <72944476+JuneMaths@users.noreply.github.com>
2020-10-23 19:14:21 +0200

    Update place_of_sign_in_string.py

--- place_of_sign_in_string.py
+++ place_of_sign_in_string.py
@@ -1,6 +1,51 @@
 # The program find a position of any sign in a string from given position to given position. 
 # It starts counting from 0.
+import matplotlib.pyplot as plt
+import numpy as np
 
+def find_the_sign(start, end, number, x, sentence):
+	''' The function...
+	'''
+	sign_position = []
+	for i in range (start, end+1):	# for don't take the last iteration
+		if  sentence[i] == x:
+			sign_position.append(i)	# Saving position of the sign counting from 0
+			number = number + 1		# Counting how many signs were found
+	# Output information
+	if number == 0:	
+		print('The sign',x, 'was not found in the sentence.')
+	else:
+		if number == 1:
+			print('Sign "', x, '" appears in the string 1 time on the position',
+				sign_position,'counting from 0.')
+		else:
+			print('Sign "', x, '" appears in the string', 
+				number, 'times on the position',
+				sign_position,'counting from 0.')	
+	return None		
+		
+		
+def histogram_sign(sentence):
+	''' The function...
+	'''
+	print(sentence)
+	sign_list = []
+	for i in sentence:	# Filters existing signs
+		if i not in sign_list:
+			sign_list.append(i)
+	amount = []
+	for i in sign_list: # Amount of every sign
+		amount.append(sentence.count(i))
+	plt.bar( np.arange(len(sign_list)),amount, facecolor='black', edgecolor='grey')
+	plt.xticks(np.arange(len(sign_list)), sign_list)
+	for i in range(0, len(sign_list)):
+		plt.text(i, amount[i]+0.08, str(amount[i]))
+	plt.title('Histogram of signs in sentence')
+	plt.xlabel('sign')
+	plt.ylabel('number of sign')
+	plt.show()
+	
+		
 sentence = input('Enter the selected string: ')
 sentence = sentence.lower()
 
@@ -16,31 +61,15 @@ if length >= 0:			# Sentence have to be entered
 			end = int(input('Enter the position where you want to end searching: '))		
 		except:
 			raise TypeError('you should enter an integer')
-		
+
 		# Main condition and mechanism
 		if start <= end and end <= length and start >= 0: # 0 <= start <= end <= length
-			sign_position = []
-			for i in range (start, end+1):	# for don't take the last iteration
-				if  sentence[i] == x:
-					sign_position.append(i)	# Saving position of the sign counting from 0
-					number = number + 1		# Counting how many signs were found
+			find_the_sign(start, end, number, x, sentence)
+			histogram_sign(sentence)
 		else:
 			raise ValueError('you are out of scale')
-		
-		# Output information
-		if number == 0:	
-			print('The sign',x, 'was not found in the sentence.')
-		else:
-			if number == 1:
-				print('Sign', x, 'appears in the string', 
-					sentence.count(x.lower()), 'time on the position',
-					sign_position,'counting from 0.')
-			else:
-				print('Sign', x, 'appears in the string', 
-					sentence.count(x.lower()), 'times on the position',
-					sign_position,'counting from 0.')
-					
 	else:
-		raise ValueError('you can enter only one sign')
+		raise ValueError('you can enter only one sign')	
 else:
 	raise ValueError('you should enter a sentence')
+	
